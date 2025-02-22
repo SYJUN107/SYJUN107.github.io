76dbc811043db8829f56c2bd36be64f639681b96
MARTIN SHAO HE <100451260@alumnos.uc3m.es>
2025-02-22 01:58:28 +0100

    shooting stars

--- assets/scripts/shooting_stars.js
+++ assets/scripts/shooting_stars.js
@@ -4,13 +4,14 @@ function createShootingStar() {
     star.classList.add('shooting-star');
     
     // La posición vertical se define en la parte superior (dentro del 25% superior de la ventana)
-    star.style.top = Math.random() * (window.innerHeight * 0.25) + 'px';
+    star.style.top = Math.random() * (window.innerHeight * 0.20) + 'px';
     
     // La posición horizontal se define entre el 25% y el 100% del ancho de la ventana
-    star.style.left = window.innerWidth * 0.25 + Math.random() * (window.innerWidth * 0.75) + 'px';
+    star.style.left = window.innerWidth * 0.35 + Math.random() * (window.innerWidth * 0.65) + 'px';
     
     // Randomize the animation duration for variety (0.8s to 2.0s)
-    const duration = 0.8 + Math.random() * 1.2;
+    // const duration = 0.8 + Math.random() * 1.2;
+    const duration = 2
     star.style.animationDuration = duration + 's';
     
     // Append the star to the container
